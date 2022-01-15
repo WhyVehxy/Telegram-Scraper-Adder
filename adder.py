c15378820afb772da80891debb8efd5a615745f8
WhyVehxy <91931367+WhyVehxy@users.noreply.github.com>
2022-01-15 00:19:00 -0600

    Update adder.py

--- adder.py
+++ adder.py
@@ -20,8 +20,7 @@ print (gr+" ║ ├┤ │  ├┤ ║ ╦  ╠═╣ ││ ││├┤ ├┬
 print (re+" ╩ └─┘┴─┘└─┘╚═╝  ╩ ╩─┴┘─┴┘└─┘┴└─")
 
 print (cy+"version : 1.01")
-print (cy+"Make sure you Subscribed Termux Professor On Youtube")
-print (cy+"www.youtube.com/c/TermuxProfessorYT")
+print (cy+"Join @VehxysServices on Telegram")
 
 print (re+"NOTE :")
 print ("1. Telegram only allow to add 200 members in group by one user.")
